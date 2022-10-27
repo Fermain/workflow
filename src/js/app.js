7d078aa3f5975da29e32ac49e9db66e2eda78803
Fermain <Fermain@users.noreply.github.com>
2022-10-27 12:14:22 +0200

    Creates cat app

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+const apiURL = "https://nf-api.onrender.com/api/v1/cat-facts/";
+
+// const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NSwibmFtZSI6Ik9saXZlciIsImVtYWlsIjoib2xpdmVyLmRpcHBsZUBub3JvZmYubm8iLCJhdmF0YXIiOiJodHRwczovL2kuaW1ndXIuY29tL0xtNWdSdnIuanBlZyIsImJhbm5lciI6bnVsbCwiaWF0IjoxNjYzNzg3NDk4fQ.QRxXm57t6leq0b1LVxQMtz3Xx2_OuI5isS8mgHioJYw";
+
+export async function app() {
+  const response = await fetch(apiURL);
+
+  const facts = await response.json();
+  
+  facts.forEach(fact => {
+    const item = document.createElement("div");
+    item.innerText = fact.text;
+    document.body.append(item)
+  })
+
+  return "App Ready!"
+}
+
+app().then(console.log);
